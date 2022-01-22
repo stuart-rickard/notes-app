413e174d7b0b9cbe8cdd7c283af6a9bf031c1c15
stuart-rickard <stuart@bau-dev.com>
2022-01-22 10:56:10 -0800

    rough working version

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require( 'express' );
 
+const { addNewNote } = require( './lib/db-calls' );
 
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -21,24 +22,40 @@ app.use(express.static('public'));
 
 
 app.get('/api/notes', (req, res) => {
-  console.log( 'test api notes' );
+  console.log( 'inside get / api notes' );
   // console.log( req );
   // res.send('Hello!');
   res.send( notes );
 });
 
+app.get('/notes', (req, res) => {
+  console.log( 'inside get /notes' );
+  // console.log( req );
+  // res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile('/Users/stuar/Documents/miniature-eureka/public/notes.html');
+});
+
+app.post('/api/notes', (req, res) => {
+  console.log( 'inside /notes post' );
+  console.log( req.body );
+  addNewNote( req.body );
+  res.send( 'yes you are talking with /api/notes post route' )
+})
+
 app.get('/', (req, res) => {
   console.log( 'test /' );
   console.log( req );
-  res.sendFile(path.join(__dirname, './public/assets/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('*', (req, res) => {
   console.log( 'test *' );
-  console.log( req );
-  res.sendFile(path.join(__dirname, './public/assets/index.html'));
+  // console.log( req );
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`API server is listening on port ${PORT}.`);
 });
+
+console.log('dir name is: ' + __dirname);
